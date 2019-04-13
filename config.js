7285a6e298d19c2e6fc3a4cce704c693ba602648
Wallace Coelho <wac.almeida@gmail.com>
2019-04-13 19:56:06 -0300

    Add AWS amplify

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "gpms-courses-uploads",
+    BUCKET: "us-east-1"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2da4t2h85e.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_5bRP2JnZJ",
+    APP_CLIENT_ID: "34si8gq4bill9bblqvmns5jtpc",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+  }
+};
