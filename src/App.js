93c6e42645f5145f2cd494dd00a551585e6da859
Wallace Coelho <wac.almeida@gmail.com>
2019-04-13 20:34:08 -0300

    Manage project structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Routes from './Routes';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App container">
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+        <Link to="/">Courses</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Hello world asd asdasdasdas dsdas das dasd asd as dsa
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+    <Routes />
+  </div>
+);
 
 export default App;
